87ebfa9b78595178115925a4c5ff6657ad3374d5
ali salarhosseini <asalar991@gmail.com>
2020-02-20 12:03:08 +0330

    Updates

---
+++ precache-manifest.58e76328da9bd733175a655dbbea0a34.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "47568f22d7382b29abccbcdb3113201f",
+    "url": "/hello_world/index.html"
+  },
+  {
+    "revision": "e1307577ac0a6fad463e",
+    "url": "/hello_world/static/css/2.ed45abbb.chunk.css"
+  },
+  {
+    "revision": "096fe9281c0ff9d35f3f",
+    "url": "/hello_world/static/css/main.f38963d9.chunk.css"
+  },
+  {
+    "revision": "e1307577ac0a6fad463e",
+    "url": "/hello_world/static/js/2.bb8a9377.chunk.js"
+  },
+  {
+    "revision": "871d127e9d1ab7cd8eac85120afed4c4",
+    "url": "/hello_world/static/js/2.bb8a9377.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "096fe9281c0ff9d35f3f",
+    "url": "/hello_world/static/js/main.48a8229e.chunk.js"
+  },
+  {
+    "revision": "63ebd286c3454427f980",
+    "url": "/hello_world/static/js/runtime-main.25a4d3ed.js"
+  }
+]);
